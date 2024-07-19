c0f247dcdddc46335d200c50c5da73bfa836c2d7
chinpeerapat <peerapat.chiaprasert@gmail.com>
2024-07-19 22:01:21 +0700

    update

--- app/dashboard/documents/[documentId]/chat-panel.tsx
+++ app/dashboard/documents/[documentId]/chat-panel.tsx
@@ -21,6 +21,7 @@ export default function ChatPanel({
         </div>
         {chats?.map((chat) => (
           <div
+            key={chat.id} // Add the key prop here
             className={cn(
               {
                 "dark:bg-slate-800 bg-slate-200": chat.isHuman,
